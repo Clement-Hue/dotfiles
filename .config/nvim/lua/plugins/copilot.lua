ed31207193a523e1cee4306af30e83f740227d69
Clement Hue <clementhue@hotmail.fr>
2026-04-08 22:19:06 +0200

    Add copilot.lua

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,20 @@
 return {
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
     event = "InsertEnter",
-  }
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          dismiss = "<C-x>",
+          next = "<M-n>",
+          prev = "<M-p>",
+        },
+      },
+      panel = { enabled = false },
+    },
+  },
 }
